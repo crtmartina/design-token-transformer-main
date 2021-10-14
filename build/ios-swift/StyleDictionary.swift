3b608fc3f2a10dbdd3be621a2b96ab84ddfcca5d
Martina Carta <martina.carta@sketchin.com>
2021-10-14 10:21:35 +0200

    first commit

---
+++ build/ios-swift/StyleDictionary.swift
@@ -0,0 +1,115 @@
+
+//
+// StyleDictionary.swift
+//
+
+// Do not edit directly
+// Generated on Wed, 13 Oct 2021 15:16:50 GMT
+
+
+import UIKit
+
+public class StyleDictionary {
+    public static let colorExtendedDarkNeutralsBase0exd1 = UIColor(red: 0.102, green: 0.137, blue: 0.247, alpha: 1)
+    public static let colorExtendedDarkNeutralsBase0exd2 = UIColor(red: 0.000, green: 0.165, blue: 0.298, alpha: 1)
+    public static let colorExtendedDarkNeutralsBase0exd3 = UIColor(red: 0.216, green: 0.267, blue: 0.380, alpha: 1)
+    public static let colorExtendedLightNeutralsBase0exl1 = UIColor(red: 0.647, green: 0.655, blue: 0.698, alpha: 1)
+    public static let colorExtendedLightNeutralsBase0exl2 = UIColor(red: 0.808, green: 0.812, blue: 0.835, alpha: 1)
+    public static let colorExtendedLightNeutralsBase0exl3 = UIColor(red: 0.984, green: 0.984, blue: 0.984, alpha: 1)
+    public static let colorExtendedMidNeutralsBase0exm1 = UIColor(red: 0.318, green: 0.373, blue: 0.475, alpha: 1)
+    public static let colorExtendedMidNeutralsBase0exm2 = UIColor(red: 0.373, green: 0.416, blue: 0.494, alpha: 1)
+    public static let colorInteractiveBase00ia = UIColor(red: 0.000, green: 0.459, blue: 0.839, alpha: 1)
+    public static let colorInteractiveBase00id = UIColor(red: 0.000, green: 0.502, blue: 0.922, alpha: 1)
+    public static let colorInteractiveBase00id1 = UIColor(red: 0.855, green: 0.914, blue: 0.965, alpha: 1)
+    public static let colorInteractiveBase00ih = UIColor(red: 0.000, green: 0.404, blue: 0.741, alpha: 1)
+    public static let colorInteractiveBase01ia = UIColor(red: 1.000, green: 0.890, blue: 0.180, alpha: 1)
+    public static let colorInteractiveBase01id = UIColor(red: 1.000, green: 0.933, blue: 0.333, alpha: 1)
+    public static let colorInteractiveBase01ih = UIColor(red: 1.000, green: 0.941, blue: 0.420, alpha: 1)
+    public static let colorInteractiveBase0ix = UIColor(red: 0.878, green: 0.898, blue: 0.922, alpha: 1)
+    public static let colorInteractiveBase0ix1 = UIColor(red: 0.431, green: 0.455, blue: 0.514, alpha: 1)
+    public static let colorPrimaryBase00 = UIColor(red: 0.000, green: 0.361, blue: 0.663, alpha: 1)
+    public static let colorPrimaryBase01 = UIColor(red: 0.976, green: 0.914, blue: 0.357, alpha: 1)
+    public static let colorSecondaryBase10 = UIColor(red: 0.000, green: 0.271, blue: 0.502, alpha: 1)
+    public static let colorSecondaryBase11 = UIColor(red: 0.714, green: 0.831, blue: 0.929, alpha: 1)
+    public static let colorSecondaryBase12 = UIColor(red: 0.855, green: 0.914, blue: 0.965, alpha: 1)
+    public static let colorSecondaryBase13 = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let colorSemanticBase0sc = UIColor(red: 0.408, green: 0.976, blue: 0.569, alpha: 1)
+    public static let colorSemanticBase0sd = UIColor(red: 0.859, green: 0.937, blue: 0.984, alpha: 1)
+    public static let colorSemanticBase0sn = UIColor(red: 0.400, green: 1.000, blue: 1.000, alpha: 1)
+    public static let colorSemanticBase0sp = UIColor(red: 1.000, green: 0.773, blue: 0.192, alpha: 1)
+    public static let colorSemanticBase0sr = UIColor(red: 1.000, green: 0.424, blue: 0.424, alpha: 1)
+    public static let colorSemanticBase0sv = UIColor(red: 0.431, green: 0.290, blue: 1.000, alpha: 1)
+    public static let colorSemanticBase0sw = UIColor(red: 0.000, green: 0.165, blue: 0.298, alpha: 1)
+    public static let ctaCta0md03 = [object Object]
+    public static let ctaCta0xl04 = [object Object]
+    public static let fontFamiliesRubik = Rubik
+    public static let fontSize0 = 8
+    public static let fontSize1 = 10
+    public static let fontSize2 = 12
+    public static let fontSize3 = 14
+    public static let fontSize4 = 16
+    public static let fontSize5 = 18
+    public static let fontSize6 = 24
+    public static let fontSize7 = 32
+    public static let fontSize8 = 36
+    public static let fontSize9 = 62
+    public static let fontWeightsRubik0 = Medium
+    public static let fontWeightsRubik1 = Regular
+    public static let fontWeightsRubik2 = SemiBold
+    public static let fontWeightsRubik3 = Bold
+    public static let gradientWhite = linear-gradient(0deg, #ffffff 0%, #ffffff 65.61%, #ffffff 65.62%, #ffffff 100%)
+    public static let h1MediumH1md10 = [object Object]
+    public static let h1MediumH1md11 = [object Object]
+    public static let h1SemiboldH1sb10 = [object Object]
+    public static let h1SemiboldH1sb11 = [object Object]
+    public static let h2RegularH2rg21 = [object Object]
+    public static let h2RegularH2rg22 = [object Object]
+    public static let h3SemiboldH3sb31 = [object Object]
+    public static let labelBoldLabel0bd00 = [object Object]
+    public static let labelBoldLabel0bd01 = [object Object]
+    public static let labelBoldLabel0bd02 = [object Object]
+    public static let labelBoldLabel0bd03 = [object Object]
+    public static let labelBoldLabel0bd04 = [object Object]
+    public static let labelBoldLabel0bd05 = [object Object]
+    public static let labelMediumLabel0md00 = [object Object]
+    public static let labelMediumLabel0md01 = [object Object]
+    public static let labelMediumLabel0md02 = [object Object]
+    public static let labelMediumLabel0md03 = [object Object]
+    public static let labelMediumLabel0md04 = [object Object]
+    public static let labelMediumLabel0md05 = [object Object]
+    public static let labelRegularLabel0rg00 = [object Object]
+    public static let labelRegularLabel0rg01 = [object Object]
+    public static let labelRegularLabel0rg02 = [object Object]
+    public static let labelRegularLabel0rg03 = [object Object]
+    public static let labelRegularLabel0rg04 = [object Object]
+    public static let labelRegularLabel0rg05 = [object Object]
+    public static let letterSpacing0 = 0%
+    public static let letterSpacing1 = 13%
+    public static let lineHeights0 = 21
+    public static let lineHeights1 = 18
+    public static let lineHeights10 = 36
+    public static let lineHeights11 = 27
+    public static let lineHeights12 = 22
+    public static let lineHeights13 = 17
+    public static let lineHeights2 = 16
+    public static let lineHeights3 = 14
+    public static let lineHeights4 = 11
+    public static let lineHeights5 = 9
+    public static let lineHeights6 = 24
+    public static let lineHeights7 = 70
+    public static let lineHeights8 = 41
+    public static let lineHeights9 = 44
+    public static let linkUnderlineMediumLink0md00 = [object Object]
+    public static let linkUnderlineMediumLink0md01 = [object Object]
+    public static let linkUnderlineMediumLink0md02 = [object Object]
+    public static let linkUnderlineMediumLink0md03 = [object Object]
+    public static let linkUnderlineMediumLink0md04 = [object Object]
+    public static let linkUnderlineMediumLink0md05 = [object Object]
+    public static let pMediumP0md05 = [object Object]
+    public static let pMediumP0rg06 = [object Object]
+    public static let pRegualrP0rg02 = [object Object]
+    public static let pRegualrP0rg04 = [object Object]
+    public static let pRegualrP0rg05 = [object Object]
+    public static let pRegualrP0rg06 = [object Object]
+    public static let paragraphSpacing0 = 0
+}
